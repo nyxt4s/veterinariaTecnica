f95dd350fececfec47cd1788c7c8418b8a26a6d4
nyxt4s <garaynyxmedina@gmail.com>
2023-06-08 16:54:22 -0400

    add: se agrega methods al cors

--- src/app.js
+++ src/app.js
@@ -16,9 +16,11 @@ const port = process.env.PORT || 3000 ;
 
 app.use(express.json());
 var corsOptions = {
-    origin: 'http://127.0.0.1:5173/',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    origin: 'http://127.0.0.1:5173/persona',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
   }
+
 
 app.use(cors(corsOptions));
 
